1fde2a270fdc1fda78c0573bf7c55ea6825500f6
nathan <nathan+dev@electriccoin.co>
2019-12-03 16:14:18 -0800

    Add explicit comparison that shows the diff; reveals the test vector bug; test is nondeterministic.

--- tests/widget_tests.rs
+++ tests/widget_tests.rs
@@ -2,21 +2,18 @@
 fn test_single_empty_child() -> std::io::Result<()> {
     run_single_child(
         "\
-         Ok(Outline(\"1, 2, 3\\n\"))\n\
+         Ok(OutLine(\"1, 2, 3\\n\"))\n\
          Ok(Status(ExitStatus(ExitStatus(0))))\n\
          ",
     )
 }
 
 fn run_single_child<S: AsRef<str>>(expected: S) -> std::io::Result<()> {
-    use std::collections::hash_map::RandomState;
     use std::collections::HashSet;
     use std::os::unix::process::ExitStatusExt;
 
-    let rs = RandomState::new();
-
-    fn lineset_of<'a>(rs: &RandomState, text: &'a str) -> HashSet<&'a str> {
-        let mut set = HashSet::with_hasher(rs.clone());
+    fn lineset_of(text: &str) -> HashSet<&str> {
+        let mut set = HashSet::new();
 
         for line in text.split('\n') {
             set.insert(line);
@@ -25,21 +22,27 @@ fn run_single_child<S: AsRef<str>>(expected: S) -> std::io::Result<()> {
         set
     }
 
-    let expectedset = lineset_of(&rs, expected.as_ref());
+    fn assert_sets_equal(a: HashSet<&str>, b: HashSet<&str>) {
+        let diff: Vec<&str> = a.symmetric_difference(&b).map(|sref| *sref).collect();
+        assert!(
+            diff.is_empty(),
+            "\nThese output lines don't match:\n  {}\n",
+            diff.join("\n  "),
+        );
+    }
+
+    let expectedset = lineset_of(expected.as_ref());
 
     let output = std::process::Command::new("target/debug/iomux")
         .arg("target/debug/iomux-testwidget")
         .arg("3")
         .output()?;
 
-    let actualset = lineset_of(
-        &rs,
-        std::str::from_utf8(&output.stdout[..]).expect("utf8 output"),
-    );
+    let actualset = lineset_of(std::str::from_utf8(&output.stdout[..]).expect("utf8 output"));
 
     assert_eq!(std::process::ExitStatus::from_raw(0), output.status);
-    assert_eq!(&expectedset, &actualset);
     assert_eq!(Vec::<u8>::new(), output.stderr);
+    assert_sets_equal(expectedset, actualset);
 
     Ok(())
 }
