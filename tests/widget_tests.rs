5b55428b1b4624e9abf9ba6c03aa84219259a337
nathan <nathan+dev@electriccoin.co>
2019-12-03 16:00:35 -0800

    Attempt to compare outputs with `HashSet` instances with the same cloned `RandomState`. Does not work.

--- tests/widget_tests.rs
+++ tests/widget_tests.rs
@@ -9,11 +9,14 @@ fn test_single_empty_child() -> std::io::Result<()> {
 }
 
 fn run_single_child<S: AsRef<str>>(expected: S) -> std::io::Result<()> {
+    use std::collections::hash_map::RandomState;
     use std::collections::HashSet;
     use std::os::unix::process::ExitStatusExt;
 
-    fn lineset_of(text: &str) -> HashSet<&str> {
-        let mut set = HashSet::new();
+    let rs = RandomState::new();
+
+    fn lineset_of<'a>(rs: &RandomState, text: &'a str) -> HashSet<&'a str> {
+        let mut set = HashSet::with_hasher(rs.clone());
 
         for line in text.split('\n') {
             set.insert(line);
@@ -22,17 +25,20 @@ fn run_single_child<S: AsRef<str>>(expected: S) -> std::io::Result<()> {
         set
     }
 
-    let expectedset = lineset_of(expected.as_ref());
+    let expectedset = lineset_of(&rs, expected.as_ref());
 
     let output = std::process::Command::new("target/debug/iomux")
         .arg("target/debug/iomux-testwidget")
         .arg("3")
         .output()?;
 
-    let actualset = lineset_of(std::str::from_utf8(&output.stdout[..]).expect("utf8 output"));
+    let actualset = lineset_of(
+        &rs,
+        std::str::from_utf8(&output.stdout[..]).expect("utf8 output"),
+    );
 
     assert_eq!(std::process::ExitStatus::from_raw(0), output.status);
-    assert_eq!(expectedset, actualset);
+    assert_eq!(&expectedset, &actualset);
     assert_eq!(Vec::<u8>::new(), output.stderr);
 
     Ok(())
